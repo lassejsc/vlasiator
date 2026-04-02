731d82471b37fba6746552dc5996daef6a5b1e07
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-04-02 10:55:19 +0300

    Added static back to adds in rp.h because this should not be an issue

--- readparameters.h
+++ readparameters.h
@@ -60,7 +60,7 @@ public:
    //           desc.c_str());
    //    }
    // }
-    template <typename T> void add_each_lambda(const std::string& name, const std::string& desc, T& defValue,
+    template <typename T> static void add_each_lambda(const std::string& name, const std::string& desc, T& defValue,
         std::function<void(const std::string)> lambda
                                                     ) {
       int rank;
@@ -102,7 +102,7 @@ public:
   //            desc.c_str())->each(lambda);
   //     }
   //  }
-   template <typename T> void add(const std::string& name, const std::string& desc,  T& defValue) {
+   template <typename T> static void add(const std::string& name, const std::string& desc,  T& defValue) {
       int rank;
       MPI_Comm_rank(MPI_COMM_WORLD, &rank);
       if (rank == MASTER_RANK) {
