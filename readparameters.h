89a313c4fcf7f39027581d4826aed3c6d8b2da5c
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-04-08 13:55:11 +0300

    through all the pain and suffering

--- readparameters.h
+++ readparameters.h
@@ -90,14 +90,14 @@ public:
           };
           if (sub!=nullptr)
           {
-            sub->add_option(namein.c_str(), defValue, desc.c_str())->each(lambda)->expected(0);
+            sub->add_option(("--"+namein).c_str(), defValue, desc.c_str())->each(lambda)->expected(0);
             isOptionParsed[subcom]=true;
           } else {
           std::cerr << "Something went wrong with adding subcommand "+subcom+"!" << std::endl;
           abort();
            };
         } else {
-          app->add_option(name.c_str(), defValue, desc.c_str())->each(lambda)->expected(0);
+          app->add_option(("--"+name).c_str(), defValue, desc.c_str())->each(lambda)->expected(0);
         }  
          // options[name] = "";
          // isOptionParsed[name] = false;
@@ -156,7 +156,7 @@ public:
           };
           if (sub!=nullptr)
           {
-            sub->add_option(namein.c_str(), defValue, desc.c_str())->expected(0)->capture_default_str(); //->each(lambda);
+            sub->add_option(("--"+namein).c_str(), defValue, desc.c_str())->expected(0)->capture_default_str(); //->each(lambda);
             isOptionParsed[subcom]=true;
           } else {
           std::cerr << "Something went wrong with adding subcommand "+subcom+"!" << std::endl;
